666d0e47d9d2e71abfc253ac3c6533b8d7d54a1b
Jignyas <gjignyassr@gmail.com>
2026-02-07 13:26:01 -0800

    feat: add staging model with json flattening and deduplication tests

---
+++ reddit_dbt/models/staging/stg_reddit_posts.sql
@@ -0,0 +1,19 @@
+-- models/staging/stg_reddit_posts.sql
+
+with source as (
+    select * from {{ source('reddit', 'reddit_posts') }}
+),
+
+renamed as (
+    select
+        raw_data:title::string as title,
+        raw_data:author::string as author,
+        raw_data:link::string as post_url,
+        raw_data:published::string as published_raw,
+        try_to_timestamp(raw_data:published::string) as published_date
+    from source
+)
+
+select * from renamed
+-- This allows us to group by URL and pick the 1st row
+QUALIFY ROW_NUMBER() OVER (PARTITION BY post_url ORDER BY published_date DESC) = 1
